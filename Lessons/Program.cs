3027569842722b870008eca716f8ab09652bb972
AndreyKolesnikovg <zergrushovermind@gmail.com>
2023-02-19 19:19:02 +0700

    Video5

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -1,35 +1,14 @@
 ﻿using System;
-
-//
-
 namespace Lessons
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a, b;
-         
-            a = 6;
-            b = 8;
-
-            bool variable = false;
-            bool Variable = true;
-
-            double g = 1.33;
-            char c = 'a';
-
-            a = 655;
-
-            
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(variable);
-            Console.WriteLine(Variable);
-            Console.WriteLine(g);
-            Console.WriteLine(c);
-
+           string data;
+            data = Console.ReadLine();
+            Console.WriteLine("Привет " + data + "!!!");
+                
 
         }
     }
