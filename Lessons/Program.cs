4b58aed3fac003b3b7ae1f0c644555352527d534
AndreyKolesnikovg <zergrushovermind@gmail.com>
2023-02-19 16:41:20 +0700

    Добавьте файлы проекта.

---
+++ Lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//
+
+namespace Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Привет мир"); 
+        }
+    }
+}
