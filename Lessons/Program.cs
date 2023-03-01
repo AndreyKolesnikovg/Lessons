eda76f1f4a118664ba6eda812ad92ab9f17d172c
AndreyKolesnikovg <zergrushovermind@gmail.com>
2023-03-02 01:34:08 +0700

    Video 22

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -11,20 +11,22 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            for (int i = 0;i < 4; i++) 
-            { 
-                if (i == 2)
+            
+      
+
+                for (int i = 1; i <= 3; i++)
+
                 {
-                    continue;
-
+                    Console.WriteLine("цикл 1 итерация №: " + i);
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.WriteLine(" цикл 2 итерация №: " + j);
                 }
-               
-
-                Console.WriteLine(i);
-            }
-
-           Console.ReadLine();  
-
+                }
+            
+            
+           
+        Console.ReadLine();
         }
     }
 }
